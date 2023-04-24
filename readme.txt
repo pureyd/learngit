3fdaeed7052ae88ff67fd450ca901fe09b8fc8af
steve.nong <steve.nong@dji.com>
2023-04-24 12:11:51 +0800

    firts commit

---
+++ readme.txt
@@ -0,0 +1,1 @@
+This status in commit!
---
+++ x.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdint.h>
+
+const kReturnTrue =0;
+
+int32_t main(void){
+    printf("hello world!");
+    return kReturnTrue;
+}
